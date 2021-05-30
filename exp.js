057594fe3ac4eee62ce0c8f92394546dfcc1e6fe
Madiyar Esmuhambetov <72384095+Madiyarsite@users.noreply.github.com>
2021-05-30 13:36:52 +0600

    Update exp.js

--- exp.js
+++ exp.js
@@ -1,7 +1,7 @@
 let request = new XMLHttpRequest();
 request.open('GET', 'ex.json');
 
-request.responseType = 'json';
+
 request.setRequestHeader('Content-Type', 'application/json, charset=utf-8');
 
 let data = JSON.parse(request.response);
