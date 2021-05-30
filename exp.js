83e257b5cdc289559b4aef7a3f96f9f6da8d318f
Madiyar Esmuhambetov <72384095+Madiyarsite@users.noreply.github.com>
2021-05-30 13:33:01 +0600

    Update exp.js

--- exp.js
+++ exp.js
@@ -1,18 +1,10 @@
-let kzt = document.getElementById("kzt"),
-    usd = document.getElementById("usd");
-   kzt.addEventListener('input', ()=>{
-    let request = new XMLHttpRequest();
-    request.open('GET', 'ex.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send();
-    request.addEventListener('readystatechange', function(){
-        if(request.readyState === 4 && request.status == 200){
-            let data = JSON.parse(request.response);
-            usd.value = kzt.value/data.usd;
-            console.log(data.usd);
-        }else{
-            usd.value = "errore";
-        }
-    });
-   });
-       
+let request = new XMLHttpRequest();
+request.open('GET', 'ex.json');
+
+request.responseType = 'json';
+request.setRequestHeader('Content-Type', 'application/json, charset=utf-8');
+
+let data = JSON.parse(request.response);
+request.send();
+
+console.log(data.age);
